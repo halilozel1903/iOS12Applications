eff339d4ac63956e3f3334c5d54379fec0d5ac6e
Halil Özel <halilozel1903@gmail.com>
2018-11-04 23:16:21 +0300

    Initial Commit

---
+++ BullsEyeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BullsEyeApp
+//
+//  Created by Halil Özel on 4.11.2018.
+//  Copyright © 2018 Halil Özel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
