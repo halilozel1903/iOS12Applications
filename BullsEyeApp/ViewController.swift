33e684bdf970df78d4b28eefafac3d09a772a0b3
Halil Özel <halilozel1903@gmail.com>
2018-11-07 21:35:58 +0300

    Yorum satırları eklendi.

--- BullsEyeApp/ViewController.swift
+++ BullsEyeApp/ViewController.swift
@@ -15,22 +15,34 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+
+    // Alert işlemlerinin yapıldığı fonksiyon
+    
     @IBAction func showAlert(){
         
         // print("Hello iOS 12 Programming Course")
         
+        // alert nesnesi tanımlandı.
         let alert = UIAlertController(title: "Hello World !", message: "This is my first app", preferredStyle: .alert)
         
+        // action olusturme
         let action = UIAlertAction(title: "Awsome", style: .default, handler: nil)
         
+        // alert nesnesine action ekleme
         alert.addAction(action)
         
+        // alerti ekranda gösterme
         present(alert, animated: true, completion: nil)
         
     }
     
     
-   
+    // slider işlemlerinin yapıldığı fonksiyon
+    @IBAction func sliderMoved(_ slider: UISlider){
+
+        print("The value of the slider is now : \(slider.value)")
+        
+    }
     
 
 
